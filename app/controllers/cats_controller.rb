f40b164672893ef3aa882214be1df1f7e61d70f0
Colin Frankish <colin.frankish@bcsg.com>
2017-01-30 17:11:37 +0000

    We add the update action to our controller and we now have full CRUD operation.
    modified:   app/controllers/cats_controller.rb

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -25,6 +25,15 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
   end
 
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cats_path
+    else
+      redirect_to new_cat_path
+    end
+  end
+
   def destroy
     @cat = Cat.find(params[:id])
     @cat.destroy!
