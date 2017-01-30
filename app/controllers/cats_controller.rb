75a9a4165c16d4a8752bcfccb20523a8463da15a
Colin Frankish <colin.frankish@bcsg.com>
2017-01-30 16:22:07 +0000

    Creation of cat_params strong parameters. New 'create' method to save our cat when we submit the form. Updated 'index' action to get all our saved cats. Updated 'index' view to list the saved cats.
    modified:   app/controllers/cats_controller.rb
    modified:   app/views/cats/index.html.erb

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,11 +1,24 @@
 class CatsController < ApplicationController
 
   def index
-
+    @cats = Cat.all
   end
 
   def new
     @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save cat_params
+      redirect_to cats_path
+    else
+      redirect_to new_cat_path
+    end
+  end
+
+  private def cat_params
+    params.require(:cat).permit(:name, :description)
   end
 
 end
